c6be34cb6f2e50a1d5f0980e032de0146b09539b
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-22 21:48:19 -0600

    Reminder on Building Lists

--- src/App.js
+++ src/App.js
@@ -3,16 +3,19 @@ import Accordion from "./components/Accordion";
 function App() {
   const items = [
     {
+      id: "l2kj5",
       label: "Can I use React on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "lk2j35lkj",
       label: "Can I use JavaScript on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
     },
     {
+      id: "l1kj2i0g",
       label: "Can I use CSS on a project?",
       content:
         "You can use React on any project you want. You can use React on any project you want. You can use React on any project you want. You can use React on any project you want.",
