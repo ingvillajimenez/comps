761e459568bad7ad16fe14b220ef1ab9f2844a96
Juan Villa <ing.villa.jimenez@gmail.com>
2023-09-28 23:02:20 -0600

    Hook Creation Process in Depth

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react";
+
+// Custom Hook
+function useCounter(initialCount) {
+  const [count, setCount] = useState(initialCount);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  return {
+    count,
+    increment,
+  };
+}
+
+export default useCounter;
